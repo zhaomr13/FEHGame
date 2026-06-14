67c219211bc36912dc5755e660353a6fae1620f2
Mingrui Zhao <mzhao@pOrchard>
2026-06-14 16:51:26 +0800

    refactor: split BattleUnit into tactics, combat, movement components

---
+++ godot/scripts/battle/BattleUnitMovement.gd
@@ -0,0 +1,39 @@
+class_name BattleUnitMovement
+extends Node2D
+
+@onready var battle_unit: BattleUnit = $".."
+
+func perform_melee_attack_sequence(target: BattleUnit, damage: int):
+	var original_global_pos = battle_unit.global_position
+	var target_global_pos = target.global_position
+
+	var stop_distance = 90.0
+	var approach_direction = 1.0 if target_global_pos.x > original_global_pos.x else -1.0
+	var jump_target_global = Vector2(target_global_pos.x - stop_distance * approach_direction, target_global_pos.y)
+
+	if battle_unit.character.animated_sprite.sprite_frames.has_animation("Jump"):
+		battle_unit.character.animated_sprite.play("Jump")
+	else:
+		battle_unit.character.set_state(Character.State.MOVING)
+
+	var jump_tween = battle_unit.create_tween()
+	jump_tween.set_trans(Tween.TRANS_QUAD)
+	jump_tween.set_ease(Tween.EASE_OUT)
+	jump_tween.tween_property(battle_unit, "global_position", jump_target_global, 0.3)
+	await jump_tween.finished
+
+	var anim_name = battle_unit.character.play_attack_animation()
+	if anim_name != "" and battle_unit.character.animated_sprite.sprite_frames.has_animation(anim_name):
+		await battle_unit.character.animated_sprite.animation_finished
+	else:
+		await get_tree().create_timer(0.3).timeout
+
+	await target.take_damage(damage)
+
+	await get_tree().create_timer(0.1).timeout
+
+	var return_tween = battle_unit.create_tween()
+	return_tween.set_trans(Tween.TRANS_LINEAR)
+	return_tween.set_ease(Tween.EASE_IN_OUT)
+	return_tween.tween_property(battle_unit, "global_position", original_global_pos, 0.3)
+	await return_tween.finished
